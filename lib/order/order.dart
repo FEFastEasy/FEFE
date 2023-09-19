b744ff645a2f9b493c6e83fd099950afb2145496
bctjuh7 <bctjuh7@gmail.com>
2023-09-19 16:29:47 +0900

    230918_order_menu

--- lib/order/order.dart
+++ lib/order/order.dart
@@ -12,7 +12,20 @@ class Order extends StatefulWidget {
 
 class _OrderState extends State<Order> {
   int _currentIndex = 2;
-  var category = ['신메뉴', '추천', '메인', '세트', '주류', '직원호출'];
+  var category = ['신메뉴', '추천', '메인', '세트', '주류', '호출'];
+  var menu = [
+    '생맥주',
+    '김치찌개',
+    '된장찌개',
+    '순두부찌개',
+    '고추장찌개',
+    '제육볶음',
+    '소불고기',
+    '뚝배기불고기',
+    '공기밥',
+    '소주'
+  ];
+  List<int> _quantityList = List.filled(10, 0);
 
   void _navigateToPage(int index) {
     if (index != _currentIndex) {
@@ -22,14 +35,30 @@ class _OrderState extends State<Order> {
 
       switch (index) {
         case 0:
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHome()));
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const MyHome()));
           break;
         case 4:
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Info()));
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const Info()));
           break;
         default:
           break;
       }
+    }
+  }
+
+  void _incrementQuantity(int index) {
+    setState(() {
+      _quantityList[index]++;
+    });
+  }
+
+  void _decrementQuantity(int index) {
+    if (_quantityList[index] > 0) {
+      setState(() {
+        _quantityList[index]--;
+      });
     }
   }
 
@@ -38,19 +67,43 @@ class _OrderState extends State<Order> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height / 8, // 전체 세로 화면 높이의 8분의 1,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: const Color(0xff7f7f7f),
-            ),
-            child: Center(
-              child: Text(
-                "광고",
-                style: const TextStyle(
-                  fontSize: 40,
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal, // 수평 스크롤
+            child: Row(
+              children: [
+                Container(
+                  width:
+                      MediaQuery.of(context).size.width / 1, // 광고 컨테이너의 너비 조정
+                  height: MediaQuery.of(context).size.height / 8,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff7f7f7f),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '광고1',
+                      style: const TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                Container(
+                  width:
+                      MediaQuery.of(context).size.width / 1, // 광고 컨테이너의 너비 조정
+                  height: MediaQuery.of(context).size.height / 8,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff7f7f7f),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '광고2',
+                      style: const TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Container(
@@ -71,47 +124,130 @@ class _OrderState extends State<Order> {
           Row(
             children: [
               TextButton(
-                onPressed: (){
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHome()));
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => const MyHome()));
                 },
                 child: Text("메뉴"),
               ),
               TextButton(
-                onPressed: (){
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Review()));
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => const Review()));
                 },
                 child: Text("리뷰"),
               ),
               TextButton(
-                onPressed: (){
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHome()));
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => const MyHome()));
                 },
                 child: Text("정보"),
               ),
             ],
           ),
-          Expanded( // Added Expanded
-            child: ListView( // Use ListView for Scrollable Content
+          Expanded(
+            // Added Expanded
+            child: ListView(
+              // Use ListView for Scrollable Content
               scrollDirection: Axis.horizontal, // 가로 스크롤
               children: [
                 Container(
                   width: MediaQuery.of(context).size.width / 6, // 가로 너비 지정
                   child: ListView.builder(
-                    itemCount: 10,
+                    itemCount: 6,
+                    itemExtent: 50.0, // 각 항목의 높이 설정
+                    padding: EdgeInsets.all(3),
                     itemBuilder: (context, index) {
                       return ListTile(
-                        title: Text(category[index]),
+                        title: Text(
+                          category[index],
+                          style: TextStyle(fontSize: 13),
+                          textAlign: TextAlign.center,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.white70, width: 1.0), // 테두리 추가
+                        ),
+                        onTap: () {
+                          // 탭 이벤트 처리
+                        },
                       );
                     },
                   ),
                 ),
                 Container(
-                  width: MediaQuery.of(context).size.width*5 / 6, // 가로 너비 지정
+                  width: MediaQuery.of(context).size.width * 5 / 6, // 가로 너비 지정
                   child: ListView.builder(
                     itemCount: 10,
+                    itemExtent: 100.0,
+                    padding: EdgeInsets.all(3),
                     itemBuilder: (context, index) {
                       return ListTile(
-                        title: Text('List 2 - Item $index'),
+                        title: Text(
+                          menu[index],
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        contentPadding: EdgeInsets.all(3),
+                        // 내용 주위의 패딩
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.white70, width: 1.0), // 테두리 추가
+                        ),
+                        onTap: () {
+                          // 탭 이벤트 처리
+                        },
+                        leading: Container(
+                          width: MediaQuery.of(context).size.width / 6,
+                          height: MediaQuery.of(context).size.height / 6,
+                          color: Colors.blue,
+                          child: Center(
+                            child: Text('Image'),
+                          ),
+                        ),
+                        // 다른 컨테이너 추가
+                        subtitle: Container(
+                          margin: EdgeInsets.fromLTRB(150, 45, 10, 0),
+                          height: MediaQuery.of(context).size.height / 36,
+                          decoration: BoxDecoration(
+                            color: Color(0xffFEE500),
+                            borderRadius:
+                                BorderRadius.circular(15.0), // 각을 둥글게 하는 부분
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                alignment: Alignment.center,
+                                child: IconButton(
+                                  onPressed: () => _decrementQuantity(index),
+                                  icon: Icon(Icons.remove, size: 15, color: Colors.black),
+                                  padding: EdgeInsets.zero,
+                                ),
+                              ),
+                              Container(
+                                width: 20,
+                                height: 20,
+                                alignment: Alignment.center,
+                                color: Colors.white,
+                                child: Text('${_quantityList[index]}'),
+                              ),
+                              Container(
+                                width: 20,
+                                height: 20,
+                                alignment: Alignment.center,
+                                child: IconButton(
+                                  onPressed: () => _incrementQuantity(index),
+                                  icon: Icon(Icons.add, size: 15, color: Colors.black,),
+                                  padding: EdgeInsets.zero,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       );
                     },
                   ),
@@ -122,9 +258,11 @@ class _OrderState extends State<Order> {
         ],
       ),
       bottomNavigationBar: _buildBottomNavigationBar(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
       floatingActionButton: FloatingActionButton(
-        backgroundColor: _currentIndex == 2 ? Colors.yellowAccent : Colors.yellow,
+        backgroundColor:
+            _currentIndex == 2 ? Colors.yellowAccent : Colors.yellow,
         child: const Icon(Icons.shopping_bag_outlined, color: Colors.black),
         onPressed: () {
           _navigateToPage(2);
@@ -167,7 +305,8 @@ class _OrderState extends State<Order> {
     );
   }
 
-  BottomNavigationBarItem _buildBottomNavigationBarItem(IconData icon, String label) {
+  BottomNavigationBarItem _buildBottomNavigationBarItem(
+      IconData icon, String label) {
     return BottomNavigationBarItem(
       icon: Container(
         margin: const EdgeInsets.symmetric(vertical: 1.0),
