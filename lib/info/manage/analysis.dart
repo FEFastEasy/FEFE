8f734531c3f1f3c06ece0db2abd4ddb6c83c0709
bctjuh7 <bctjuh7@gmail.com>
2023-09-26 03:52:00 +0900

    230925_analysis add chart

--- lib/info/manage/analysis.dart
+++ lib/info/manage/analysis.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class Analysis extends StatefulWidget {
-
   const Analysis({Key? key}) : super(key: key);
 
   @override
@@ -9,6 +9,12 @@ class Analysis extends StatefulWidget {
 }
 
 class _AnalysisState extends State<Analysis> {
+  int touchedIndex = -1;
+
+  static final TextStyle _textStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -32,15 +38,195 @@ class _AnalysisState extends State<Analysis> {
         backgroundColor: Color(0xfffae100),
       ),
       body: Container(
-        margin: EdgeInsets.all(2.0),
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width / 20),
         child: Column(
           children: [
+            SizedBox(height: 40),
             Container(
-              height: 80,
-            )
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.people, size: 16, color: Colors.blue),
+                      Text(' 오늘 방문고객 수 :', style: _textStyle),
+                    ],
+                  ),
+                  Text('100명 (+10명)', style: _textStyle),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.people, size: 16, color: Colors.blue),
+                      Text(' 이번달 방문고객 수 :', style: _textStyle),
+                    ],
+                  ),
+                  Text('1,000명 (+100명)', style: _textStyle),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.people, size: 16, color: Colors.blue),
+                      Text(' 올해 방문고객 수 :', style: _textStyle),
+                    ],
+                  ),
+                  Text('10,000명 (+1,000명)', style: _textStyle),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.grey),
+                borderRadius: BorderRadius.circular(5)
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Center(
+                    child: Text('고객 성별 분석',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: PieChart(
+                            PieChartData(
+                              pieTouchData: PieTouchData(
+                                touchCallback:
+                                    (FlTouchEvent event, pieTouchResponse) {
+                                  setState(() {
+                                    if (!event.isInterestedForInteractions ||
+                                        pieTouchResponse == null ||
+                                        pieTouchResponse.touchedSection ==
+                                            null) {
+                                      touchedIndex = -1;
+                                      return;
+                                    }
+                                    touchedIndex = pieTouchResponse
+                                        .touchedSection!.touchedSectionIndex;
+                                  });
+                                },
+                              ),
+                              borderData: FlBorderData(
+                                show: false,
+                              ),
+                              sectionsSpace: 0,
+                              centerSpaceRadius: 40,
+                              sections: showingSections(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Container(
+                                width: 25,
+                                height: 25,
+                                color: Colors.blue,
+                              ),
+                              Container(
+                                color: Colors.white,
+                                child: Text(' 남자',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                width: 25,
+                                height: 25,
+                                color: Colors.amber,
+                              ),
+                              Container(
+                                color: Colors.white,
+                                child: Text(' 여자',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold)),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 20),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(2, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 30.0 : 20.0;
+      final radius = isTouched ? 60.0 : 50.0;
+      const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: Colors.amber,
+            value: 40,
+            title: '40%',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: shadows,
+            ),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: Colors.blue,
+            value: 60,
+            title: '60%',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: shadows,
+            ),
+          );
+        default:
+          throw Error();
+      }
+    });
   }
 }
