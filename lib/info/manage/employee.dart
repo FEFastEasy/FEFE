b972751bfb8039ce605a6cb0b616a520a557778e
bctjuh7 <bctjuh7@gmail.com>
2023-09-27 15:43:58 +0900

    230927_employee add list

--- lib/info/manage/employee.dart
+++ lib/info/manage/employee.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Employee extends StatefulWidget {
-
   const Employee({Key? key}) : super(key: key);
 
   @override
@@ -9,6 +8,10 @@ class Employee extends StatefulWidget {
 }
 
 class _EmployeeState extends State<Employee> {
+  static final TextStyle _textStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -32,14 +35,65 @@ class _EmployeeState extends State<Employee> {
         backgroundColor: Color(0xfffae100),
       ),
       body: Container(
-        margin: EdgeInsets.all(2.0),
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-            )
-          ],
-        ),
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width / 20),
+        child: Column(children: [
+          SizedBox(height: 40),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.account_circle, size: 16, color: Colors.blue),
+                    Text(' 현재 출근 직원 수 :', style: _textStyle),
+                  ],
+                ),
+                Text('7/10명', style: _textStyle),
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Container(
+            height: 40,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(border: Border.all(width: 1), borderRadius: BorderRadius.circular(10), color: Colors.grey),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('직원01', style: _textStyle),
+                Text('출근', style: _textStyle),
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Container(
+            height: 40,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(border: Border.all(width: 1), borderRadius: BorderRadius.circular(10), color: Colors.grey),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('직원01', style: _textStyle),
+                Text('퇴근', style: _textStyle),
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Container(
+            height: 40,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(border: Border.all(width: 1), borderRadius: BorderRadius.circular(10), color: Colors.grey),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('직원01', style: _textStyle),
+                Text('휴무', style: _textStyle),
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
